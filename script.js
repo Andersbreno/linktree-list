c212a183f3ad764732dcd78ef3a893805b2c2aba
Anderson Breno <andersbreno@gmail.com>
2024-02-19 21:33:20 -0300

    change div profile img html, style border-radius #profile img 100%, alt div profile img new text, README. Add new avatars.

--- script.js
+++ script.js
@@ -3,8 +3,16 @@ function toggleMode() {
   html.classList.toggle("light")
   const img = document.querySelector("#profile img")
   if (html.classList.contains("light")) {
-    img.setAttribute("src", "./assets/avatar-light.png")
+    img.setAttribute("src", "./assets/avatar-light.jfif")
+    img.setAttribute(
+      "alt",
+      "Foto de Anderson Breno sorrindo, vestindo camisa social preta e janelas ao fundo"
+    )
   } else {
-    img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute("src", "./assets/avatar.jfif")
+    img.setAttribute(
+      "alt",
+      "Foto de Anderson Breno em um elevador, segurando o celular vestido de camisa social verde e crachá"
+    )
   }
 }
